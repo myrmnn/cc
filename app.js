d08a9dc8f74b2f818af70394a965fcda34712bac
myrmnn <myrm.nn@gmail.com>
2021-03-06 14:45:22 -0500

    changed menu icon

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".navbar");
 
-menu.addEventListener("click", () => nav.classList.toggle("show"));
+menu.addEventListener("click", () => {
+	nav.classList.toggle("show");
+	menu.classList.toggle("open");
+});
