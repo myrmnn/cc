eb89ae3946168d7a1a833d361e8833342cf0b69b
myrmnn <myrm.nn@gmail.com>
2021-03-09 11:58:20 -0500

    added navigation animations

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".navbar");
+const topOfNav = nav.offsetTop;
+const navLogo = document.querySelector(".logo");
 
 menu.addEventListener("click", () => {
 	nav.classList.toggle("show");
 	menu.classList.toggle("open");
 });
+
+function fixedNav() {
+	if (window.innerWidth >= 1000 && window.scrollY > topOfNav) {
+		nav.classList.add("fixedNav");
+		navLogo.classList.add("show");
+		document.body.style.paddingTop = nav.offsetHeight + "px";
+	} else {
+		nav.classList.remove("fixedNav");
+		document.body.style.paddingTop = 0;
+		navLogo.classList.remove("show");
+	}
+}
+
+window.addEventListener("scroll", fixedNav);
